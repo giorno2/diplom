dc7c561a12b1b95e8964de6d004f2fa3be912ee2
horuno2020 <54189384+giorno2@users.noreply.github.com>
2024-06-11 06:53:11 +0700

    ...

--- data/www/test/htdocs/send.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include("sql.php");
-$name = $_POST["name"];
-$number = $_POST["number"];
-
-send($name, $number);
-?>
--- data/www/test/htdocs/sql.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$conn = mysqli_connect("127.0.0.1","root","", "testbd");
-
-function send($name, $number){
-    global $conn;
-    $sql = "INSERT INTO `tst`(`name`, `number`) VALUES ($name,$number)";
-    $result = mysqli_query($conn, $sql);
-}
-?>
